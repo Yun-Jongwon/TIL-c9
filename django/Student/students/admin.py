21494c6b1990c64661eaf0679e2015ba5329bdfe
jongwonYun <dmdmdkdk@daum.net>
2019-01-28 08:56:06 +0000

    d19 | 190128 PM | Django CR

---
+++ django/Student/students/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import student
+
+
+
+# Register your models here.
+
+
+
+# Register your models here.
+
+admin.site.register(student)
+
---
+++ django/crud/posts/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    
+    def __str__(self):
+        return self.title
+    
+#1.Create
+#post=Post(title='hello',content='world!')
+#post.save()
+
+#2.Read
+#2.1. All
+#posts=Post.object.all()
+
+#2.2 Get one
+#post=Post.objects.get(pk=1)
+
+#2.3 filter(WHERE)
+#조건 맞는것 전부 ( 리스트 형태로 결과가 나옴)
+#posts=Post.objects.filter(title='Hello').all()
+#첫번째꺼 하나만
+#post=Post.objects.filter(title='Hello').first()
+
+#2.4. LIKE
+#posts=Post.objects.filter(title__contains="He").all()
+
+#2.5. order_by(정렬),오름차순
+#posts=Post.objects.order_by('title').all()
+#내림차순
+#posts=Post.objects.order_by('-title').all()
+
+#2.6.LIMIT
+#Post.objects.all()[:1]
+
+#3.DELTE      
+#post=Post.objects.get(pk=2)
+#post.title ->2번째
+#post.delete()
+
+#4.Update 수정
+#post=Post.objects.get(pk=1)
+#post.title ->hello
+#post.title='hi'
+#post.save()
+#post=Post.objects.get(pk=1)
+#post.title -> hi
