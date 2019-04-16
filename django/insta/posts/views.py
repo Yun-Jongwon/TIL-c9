a03db67b31daf5cd9fabcd971cc635bcc1df1520
jongwonYun <dmdmdkdk@daum.net>
2019-04-16 06:39:32 +0000

    190416 | Insta - Profile, Follow

--- django/insta/posts/views.py
+++ django/insta/posts/views.py
@@ -4,9 +4,28 @@ from django.views.decorators.http import require_POST, require_http_methods
 from .forms import PostForm,CommentForm,ImageFormSet
 from .models import Post,Comment
 from django.db import transaction
+from itertools import chain
 
-def list(request):
+
+def explore(request):
     posts=Post.objects.order_by('-id').all()
+    comment_form=CommentForm()
+    return render(request, 'posts/list.html',{'posts':posts,'comment_form':comment_form})
+
+
+
+
+
+@login_required
+def list(request):
+    # posts=Post.objects.order_by('-id').all()
+    # 1. 내가 follow 하고있는 사람들의 리스트
+    followings=request.user.followings.all()
+    # 2. followings에 내 id도 추가
+    followings=chain(followings,[request.user])
+    
+    # 2. 이사람들이 작성한 post들만 뽑아옴.
+    posts=Post.objects.filter(user__in=followings).order_by('-id')
     comment_form=CommentForm()
     return render(request, 'posts/list.html',{'posts':posts,'comment_form':comment_form})
 
